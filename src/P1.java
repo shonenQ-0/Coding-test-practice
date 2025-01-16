b85c0e1d76c1a28de97f03541b1cc00a7ab41b01
손현규 <shonenq00@gmail.com>
2025-01-16 12:16:36 +0900

    1. 문자 찾기 한개의 문자열과 특정 문자를 입력 받아 입력받은 문자열에 문자가 몇개 존재하는지 알아내는 문제였다. 대소문을 구분하지 않는 조건이 있었는데 문자열만 소문자로 변형해서 확인을 하는 실수가 있었다.

---
+++ src/P1.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        char c = br.readLine().charAt(0);
+
+        System.out.println(solution(s, c));
+    }
+
+    private static int solution(String s, char c) {
+        s = s.toLowerCase();
+        c = Character.toLowerCase(c);
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == c) count++;
+        }
+        return count;
+    }
+}
+

a1c5de1e18f7449791fabfc1300a84306992e14e
손현규 <shonenq00@gmail.com>
2025-01-16 18:11:19 +0900

    중복 문자 제거 문자열에서 중복된 문자를 제거하는 문제였다. set을 사용해서 해결했는데 문자열의 순서를 유지하기위해 LinkedHashSet을 사용했다.

---
+++ src/P6.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class P6 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        String answer = solution(input);
+        System.out.println(answer);
+    }
+
+    private static String solution(String input) {
+        String result = "";
+        HashSet<Character> set = new LinkedHashSet<>();
+        for (char c : input.toCharArray()){
+            set.add(c);
+        }
+        for (char x : set){
+            result += x;
+        }
+        return result;
+    }
+}

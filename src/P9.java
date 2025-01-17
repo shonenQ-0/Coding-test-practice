1c25126fefd70935e592f67d77d93c9dde9ec190
손현규 <shonenq00@gmail.com>
2025-01-17 21:15:45 +0900

    숫자만 추출 문자열을 입력받고 문자열에서 숫자를 제외한 문자를 제거 후 출력하는 문제였다. replaceAll 을 사용하여 정규식으로 문자를 제거 한 후 praseInt로 변환하여 출력하는 방식으로 해결했다.

--- src/P9.java
+++ src/P9.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class P9 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P9 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(solution(s));
+    }
+
+    private static int solution(String s) {
+        s = s.toLowerCase().replaceAll("[a-z]","");
+        return Integer.parseInt(s);
+    }
 }

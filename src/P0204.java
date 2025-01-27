c5532a551c8deeb1721906b34e1de8b62feea780
shonenqQ-0 <shonenq00@gmail.com>
2025-01-27 20:03:01 +0900

    더 간단한 반복문을 사용한 풀이

--- src/P0204.java
+++ src/P0204.java
@@ -6,9 +6,11 @@ public class P0204 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        System.out.println(fivo(N));
+        for (int i : fivo(N)){
+            System.out.print(i + " ");
+        }
     }
-
+//// 반복을 사용한 방법
 //    private static void fivo(int N) {
 //        int a = 1, b = 1;
 //        System.out.print(a);
@@ -19,10 +21,22 @@ public class P0204 {
 //            b = tmp; // 다음 연산에서의 뒷값 (f - 1)
 //        }
 //    }
-    private static int fivo(int N) {
-        if (N == 0) return 0;
-        if (N == 1) return 1;
 
-        return fivo(N - 1) + fivo(N - 2);
+/// /재귀를 사용한 방법
+//    private static int fivo(int N) {
+//        if (N == 0) return 0;
+//        if (N == 1) return 1;
+//
+//        return fivo(N - 1) + fivo(N - 2);
+//    }
+
+    private static int[] fivo(int n) {
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr;
     }
 }
